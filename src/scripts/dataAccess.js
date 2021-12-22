25c73334b8b28c92f69be50bf984e67a28c18ac4
michellecaner <michellecaner@gmail.com>
2021-12-21 19:06:11 -0600

    finished chapter 6

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -18,3 +18,20 @@ export const fetchRequests = () => {
 export const getRequests = () => {
   return applicationState.requests.map(request => ({ ...request }))
 }
+
+export const sendRequest = (userServiceRequest) => {
+  const fetchOptions = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(userServiceRequest)
+  }
+
+
+  return fetch(`${API}/requests`, fetchOptions)
+      .then(response => response.json())
+      .then(() => {
+
+      })
+}
